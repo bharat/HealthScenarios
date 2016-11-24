ed789fef1ce4827cfe5737530e03506a229a62bf
Bharat Mediratta <bharat@altschool.com>
2016-11-24 12:26:39 -0800

    Initial Commit

---
+++ HealthScenarios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HealthScenarios
+//
+//  Created by Bharat Mediratta on 11/24/16.
+//  Copyright © 2016 Bharat Mediratta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
